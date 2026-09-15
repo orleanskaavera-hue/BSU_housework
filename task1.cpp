b45c7be44edc773ebc26c704e2398027c7b16f4d
verok <verok@Veras-MacBook-Air.local>
2026-09-15 23:06:25 +0300

    Fix syntax error in error margin calculation

--- task1.cpp
+++ task1.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include<iomanip>
+#include<cmath>
 using namespace std;
 int main()
 {
     long double x;
-unsigned int k,bottom_number=1,i=0;
+unsigned int k,i=0;
 cout<<"Введите степень (под модулем) числа e : "<<endl;
 cin>>x;
 cout<<"Введите натуральное число большее единицы в которое возводится число  1/10 : "<<endl;
 cin>>k;
 long double Emax=pow(10.0 ,-int(k));
-long double component_i=1,summ=0,top_number=1;
+long double component_i=1,summ=0,top_number=1,bottom_number=1;
 
 
 while (abs(component_i)>=Emax)
@@ -23,6 +24,7 @@ component_i=top_number/bottom_number;
 }
 cout<<fixed<<setprecision(k);
 cout<<"Итоговое число: "<<summ<<endl;
-
+cout<<"Различия(погрешность) между рядом тейлора с заданной точностью и вычислением с помощью функции:  "<<endl;
+cout<<fabs(summ-exp(-x));
     return 0;
 }
